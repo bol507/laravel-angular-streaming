edefbc168d085328e60325bf26dacdfdf422ecd5
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-08-10 11:41:00 -0500

    feat: integrating template

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+
+declare var $: any;
+declare function HOME_INIT([]): any;
+declare function INIT_SWIPER([]): any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      HOME_INIT($);
+      INIT_SWIPER($);
+    }, 500);
+  }
 }
